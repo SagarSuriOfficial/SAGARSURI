a1dc9cd71d80488e7dd05f8a040867acb0ad900d
SagarSuriOfficial <sagarsuriofficial@gmail.com>
2024-03-30 19:29:03 +0530

    portfolio project added

---
+++ FrontEnd/portfolioTailwind/README.md
@@ -0,0 +1,8 @@
+# Portfolio
+
+# This Website hosted on -
+<br> 1st Varcel. https://sagarsuri.vercel.app/ <hr>
+
+<br> 2nd Netlify. https://sagarsuri.netlify.app/
+
+
---
+++ FrontEnd/portfolioTailwind/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./dist/*.html" , "./Projects/food-delivery/index.html" , "./Projects/pwskillsclone/index.html" , "./Projects/Multi-Page/index.html", "./Projects/AmazonClone/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
